c47cfcb0205b7bf479fdff0efb625ea4b5d41488
Kwon-Jisu <wltn80609@ajou.ac.kr>
2024-09-22 00:42:00 +0000

    change test, base_data_loader

--- pytorch-template/base/base_data_loader.py
+++ pytorch-template/base/base_data_loader.py
@@ -1,44 +1,29 @@
 import transformers
 import torch
 from module import dataset
+from torch.utils.data import DataLoader
+from torch.nn.utils.rnn import pad_sequence
 
+def collate_fn(batch, is_train=True):
+    data = []
+    targets = []
 
-class DataModule:
-    def __init__(self, plm_name, dataset_name, batch_size, shuffle, train_path, dev_path, test_path, predict_path, col_info, max_length):
-        super().__init__()
-        self.plm_name = plm_name
-        self.dataset_name = dataset_name
-        self.batch_size = batch_size
-        self.shuffle = shuffle
+    for item in batch:
+        if isinstance(item, tuple):  # train 데이터의 경우
+            data_tensor = item[0]
+            target_tensor = item[1]
+        else:  # test 데이터의 경우
+            data_tensor = item
+            target_tensor = None
 
-        self.train_path = train_path
-        self.dev_path = dev_path
-        self.test_path = test_path
-        self.predict_path = predict_path
+        data.append(data_tensor)
+        if is_train and target_tensor is not None:
+            targets.append(target_tensor)
 
-        self.train_dataset = None
-        self.val_dataset = None
-        self.test_dataset = None
-        self.predict_dataset = None
+    data_padded = pad_sequence(data, batch_first=True, padding_value=0)
 
-        self.tokenizer = transformers.AutoTokenizer.from_pretrained(plm_name, max_length=max_length)
-        self.col_info = col_info
-        self.setup()
-
-    def setup(self):
-            self.train_dataset = getattr(dataset, self.dataset_name)(self.train_path, self.tokenizer, self.col_info)
-            self.val_dataset = getattr(dataset, self.dataset_name)(self.dev_path, self.tokenizer, self.col_info)
-            self.test_dataset = getattr(dataset, self.dataset_name)(self.test_path, self.tokenizer, self.col_info)
-            self.predict_dataset = getattr(dataset, self.dataset_name)(self.predict_path, self.tokenizer, self.col_info)
-
-    def train_dataloader(self):
-        return torch.utils.data.DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=self.shuffle)
-
-    def val_dataloader(self):
-        return torch.utils.data.DataLoader(self.val_dataset, batch_size=self.batch_size)
-
-    def test_dataloader(self):
-        return torch.utils.data.DataLoader(self.test_dataset, batch_size=self.batch_size)
-
-    def predict_dataloader(self):
-        return torch.utils.data.DataLoader(self.predict_dataset, batch_size=self.batch_size)
+    if is_train:
+        targets = torch.stack(targets) if targets else None
+        return data_padded, targets
+    else:
+        return data_padded  # test 데이터에서는 target 없이 데이터만 반환
