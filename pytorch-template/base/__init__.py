5e92b4d9fa3a219e24fd53fdadb3133b0b80a566
김태원_T7329 <50550914+chris40461@users.noreply.github.com>
2024-09-27 22:26:31 +0900

    Delete pytorch-template directory

--- pytorch-template/base/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base_data_loader import *
-from .base_trainer import *
-from .base_dataset import *
--- pytorch-template/utils.py
+++
@@ -1,2 +0,0 @@
-def init_obj(module_name, module_args, module, *args):
-    return getattr(module, module_name)(*args, **module_args)
